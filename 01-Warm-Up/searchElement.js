7c047076ad1d6500a6d00bb34728cb486312a7fc
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2025-06-16 11:22:23 -0700

    Update searchElement.js

--- 01-Warm-Up/searchElement.js
+++ 01-Warm-Up/searchElement.js
@@ -4,15 +4,17 @@ console.log("Searching for an element from an array:");
 
 let arr = [10, 3, 5, 7, 9];
 
-function searchElement(arr, x) { 
-    for (let i = 0; i < arr.length; i++) { 
-        if (arr[i] == x) { 
-            return i;
-        }   
+function searchElement(arr, x) {
+    // Loop through the array to search for element x
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === x) {
+            return i; // Return the index if element is found
+        }
     }
 
-    return -1;
-}//searchElement
+    return -1; // Return -1 if the element is not found
+} // searchElement
 
-let result = searchElement(arr, 9);//4
-console.log(result);//4
+// Searching for the element 9 in the array
+let result = searchElement(arr, 9);
+console.log(result); // Output: 4
