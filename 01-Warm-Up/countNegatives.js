a74a81fe21fdb9436691f1bf8abef603a942507d
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2025-06-16 11:20:16 -0700

    Update countNegatives.js

--- 01-Warm-Up/countNegatives.js
+++ 01-Warm-Up/countNegatives.js
@@ -5,15 +5,16 @@ console.log("Return number of negative elements from an array:");
 let arr = [-10, 3, -5, 7, -9];
 
 function countNegativeNums(arr) {
-    let count = 0;
+    let count = 0; // Initialize count to store the number of negative elements
 
+    // Loop through the array
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < 0) {
-            count++;
+            count++; // Increment count if the element is negative
         }
     }
 
-    return count;
-}//countNegativeNums
+    return count; // Return the total count of negative numbers
+} // countNegativeNums
 
-console.log(countNegativeNums(arr));//2
+console.log(countNegativeNums(arr)); // Output: 3
